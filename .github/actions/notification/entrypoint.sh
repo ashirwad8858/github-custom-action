4611ced738910eb56d18314981bb40b4ee1062bd
ashirwadsharma <ashirwadsharma@zapbuild.com>
2024-08-07 21:59:42 +0530

    own action notification

--- .github/actions/notification/entrypoint.sh
+++ .github/actions/notification/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-python3 mail.py --smtp_server="$1" --smtp_port="$2" --username="$3" --password="$4" --from_email="$5" --to_email="$6" 
+python3 /usr/src/app/mail.py --smtp_server="$1" --smtp_port="$2" --username="$3" --password="$4" --from_email="$5" --to_email="$6" 
